75d009f47a74e53ca5c6edc8bc1ca6ad8cf1eb92
thepiguy <bhubhubarks20@gmail.com>
2022-09-09 17:08:49 +0200

    initial commit

---
+++ src/main/kotlin/me/piguy/allaybottle/AllayBottle.kt
@@ -0,0 +1,12 @@
+package me.piguy.allaybottle
+
+import net.fabricmc.api.ModInitializer
+
+@Suppress("UNUSED")
+object AllayBottle : ModInitializer {
+  private const val MOD_ID = "allayinabottle"
+
+  override fun onInitialize() {
+    println("Example mod has been initialized.")
+  }
+}
---
+++ src/main/kotlin/me/piguy/allaybottle/AllayBottleClient.kt
@@ -0,0 +1,11 @@
+package me.piguy.allaybottle
+
+import net.fabricmc.api.ClientModInitializer
+
+
+@Suppress("UNUSED")
+object AllayBottleClient: ClientModInitializer {
+  override fun onInitializeClient() {
+
+  }
+}
