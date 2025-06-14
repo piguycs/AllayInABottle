55edef45abf567f70d04d8206764ab6667352403
piguycs <piguycs@pm.me>
2025-06-14 09:28:13 +0200

    backport to 1.21.1 complete
    resolves #9

--- src/main/kotlin/nl/piguy/allaybottle/items/ModItems.kt
+++ src/main/kotlin/nl/piguy/allaybottle/items/ModItems.kt
@@ -7,26 +7,26 @@ import net.minecraft.item.Item
 import net.minecraft.item.ItemGroups
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
-import net.minecraft.registry.RegistryKey
-import net.minecraft.registry.RegistryKeys
 import net.minecraft.util.Identifier
 import nl.piguy.allaybottle.AllayInABottle.MOD_ID
 
 
 object ModItems {
-    val ALLAY_BOTTLE = register("allay_bottle", ::AllayBottleItem, Item.Settings())
+    val ALLAY_BOTTLE = register(AllayBottleItem(Item.Settings()), "allay_bottle")
 
     fun initialise() {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
             .register(ModifyEntries { itemGroup: FabricItemGroupEntries -> itemGroup.add(ALLAY_BOTTLE) })
     }
 
-    private fun register(name: String, itemFactory: (Item.Settings) -> Item, settings: Item.Settings): Item {
-        val key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, name))
-        val item = itemFactory.apply { settings.registryKey(key) }(settings)
+    private fun register(item: Item?, id: String): Item? {
+        // Create the identifier for the item.
+        val itemID = Identifier.of(MOD_ID, id)
 
-        Registry.register(Registries.ITEM, key, item)
+        // Register the item.
+        val registeredItem = Registry.register<Item?, Item?>(Registries.ITEM, itemID, item)
 
-        return item
+        // Return the registered item!
+        return registeredItem
     }
 }
